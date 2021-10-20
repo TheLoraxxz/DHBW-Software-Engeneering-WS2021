acbd888980c92ac5d70732759186457dd12ab4dd
The Loraxxz <tilmann.lorenz@mail.com>
2021-10-20 12:26:26 +0200

    Joystick Package finished the rough layout

--- src/main/java/Joystick/JoyStickFeeler.java
+++ src/main/java/Joystick/JoyStickFeeler.java
@@ -1,4 +1,17 @@
 package main.java.Joystick;
 
-public class JoyStickFeeler {
+public class JoyStickFeeler implements IFeeler{
+
+    JoyStickFeeler() {
+
+    }
+    @Override
+    public void press() {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
 }
--- src/main/java/Joystick/PressureButton.java
+++ src/main/java/Joystick/PressureButton.java
@@ -1,4 +1,28 @@
 package main.java.Joystick;
 
-public class PressureButton {
+import main.java.FLF.PositionType;
+
+public class PressureButton implements IFeeler {
+
+    private PositionType position;
+    PressureButton(PositionType pos) {
+        position = pos;
+    }
+    PressureButton(PressureButton pressureButton) {
+        this.position = pressureButton.getPosition();
+    }
+    @Override
+    public void press() {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    public PositionType getPosition() {
+        return position;
+    }
+
 }
