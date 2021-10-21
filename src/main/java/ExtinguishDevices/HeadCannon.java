85ae3efa584e03a111ca7551ec6186b7abdef69b
the_loraxxz <the_loraxxz@web.de>
2021-10-21 17:04:24 +0200

    Joystick Head Canon goes through

--- src/main/java/ExtinguishDevices/HeadCannon.java
+++ src/main/java/ExtinguishDevices/HeadCannon.java
@@ -1,13 +1,16 @@
 package main.java.ExtinguishDevices;
 
 public class HeadCannon {
+
     private KnopRoofStepsType steps;
     private Segments[] segments;
+    private MixType mixing;
 
     public HeadCannon() {
         this.steps = KnopRoofStepsType.a;
         segments = new Segments[]{
                     new Segments(true),
                     new Segments(false)};
+
     }
 }
