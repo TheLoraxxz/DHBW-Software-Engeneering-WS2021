37db7e5843c61e34c4ddd69b16d390130a4c7a22
The Loraxxz <tilmann.lorenz@mail.com>
2021-11-07 14:54:13 +0100

    knob roofs fixed (pummp out problem fixed)

--- src/main/java/ExtinguishDevices/KnopRoofStepsType.java
+++ src/main/java/ExtinguishDevices/KnopRoofStepsType.java
@@ -6,7 +6,7 @@ public enum KnopRoofStepsType {
     c(2500);
     private int value;
     private KnopRoofStepsType(int value) {
-
+        this.value = value;
     }
     public int getValue() {
         return this.value;
